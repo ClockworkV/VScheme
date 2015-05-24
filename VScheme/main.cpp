09d5f0272e3889f3aaa63837d0ab684108e4aa0a
ClockworkV <vladimir.krapp@gmail.com>
2015-05-24 20:28:26 +0300

    Working List

--- VScheme/main.cpp
+++ VScheme/main.cpp
@@ -7,8 +7,14 @@
 #include <vector>
 #include <memory>
 #include <regex>
+#include <map>
+#include <algorithm>
+#include <functional>
 
 using namespace std;
+
+wstringstream program_test(L" 10 ( hello?) (hello -5) ( hi ( one two))  ");
+wstringstream program_norwig(L"(begin (define r 10) (* pi (* r r)))");
 
 typedef wstring Token;
 typedef vector<Token>::iterator TokenItr;
@@ -30,6 +36,9 @@ vector<Token> tokenize(wistream& program)
 
 	while(itr != eos)
 	{
+		while(itr != eos && iswspace(*itr))
+			itr++;
+
 		tokens.emplace_back();
 
 		if(*itr == '(')
@@ -58,218 +67,247 @@ vector<Token> tokenize(wistream& program)
 	return tokens;
 }
 
-enum class Type
-{
-	NONE, NUMBER, SYMBOL, LIST, PROC, LAMBDA
-};
 
+
+template<typename T>
+void print(const T& x, wostream& os)
+{
+	//os << x << '\n';
+	os << x ;
+}
 
 class Context;
 
-
-struct ObjectImplBase : public enable_shared_from_this<ObjectImplBase>
+struct Expression
 {
-	virtual Type getType() const = 0;
-
-	virtual	shared_ptr<ObjectImplBase> evaluate(Context& context) = 0;
-
-	virtual void print(wostream& os) const = 0;
-};
-
-
-struct Object
-{
-	Object(const TokenItr valueToken);
-
-	Object(const Object& src):mpImpl(src.mpImpl)
-	{}
-
-	Type getType() const
-	{
-		return mpImpl->getType();
-	}
-
-	Object evaluate(Context& context) 
-	{
-		return Object(mpImpl->evaluate(context));
-	}
-
-	void print(wostream& os) const
-	{
-		mpImpl->print(os);
-	}
-
-	static const Object gEmptyList; 
-
-private:
-
-	Object(shared_ptr<ObjectImplBase> pValue)
-	{
-		mpImpl = pValue;
-	}
-
-	shared_ptr<ObjectImplBase> mpImpl;
-};
-
-const Object Object::gEmptyList = Object(nullptr);
-
-
-
-
-template<Type T>
-struct ObjectImpl : public ObjectImplBase 
-{
-	Type getType() const override { return T; }
-};
-
-struct Int : public ObjectImpl<Type::NUMBER>
-{
-	Int(int value):mValue(value)
-	{}
-
-	shared_ptr<ObjectImplBase> evaluate(Context& context) override
-	{
-		return shared_from_this();
-	}
-
-	void print(wostream& os) const override
-	{
-		os << mValue;
-	}
-
-
-
-private:
-	int mValue;
-};
-
-
-struct Symbol : public ObjectImpl<Type::SYMBOL>
-{
-	Symbol(wstring symbol) : mValue(symbol)
-	{}
-
-	shared_ptr<ObjectImplBase> evaluate(Context& context) override
-	{
-		return shared_from_this();
-	}
-
-	void print(wostream& os) const override
-	{
-		os << mValue;
-	}
-
-private:
-	const wstring mValue;
-};
-
-
-struct List : public ObjectImpl<Type::LIST>
-{
-
-	List(){}
-
-	shared_ptr<ObjectImplBase> evaluate(Context& context) override
-	{
-		return nullptr;
-	}
-
-	void print(wostream& os) const override
+	template<typename T>
+	Expression(const T& src):Expression_(new Impl<T>(src))
 	{
 	}
 
-	void Add(Object object)
+	Expression(const Expression& src):Expression_(src.Expression_){}
+
+	friend void print(const Expression& x, wostream& os) 
 	{
-		mList.push_back(object);
+		x.Expression_->print_(os);
 	}
 
-	Object Car() const
+	template<typename T>
+	T get() const
 	{
-		if(mList.size() >= 1)
+		auto impl = dynamic_cast<Impl<T>*>(Expression_.get());
+		if(impl)
 		{
-			return mList.front();
+			return impl->mData;
 		}
-		else
-		{
-			return Object(make_shared<List>());
-		}
+
+		throw;
 	}
 
 private:
-	vector<Object> mList;
+	struct Concept
+	{
+		virtual void print_(wostream&) const = 0;
+		virtual Expression eval(const Context& context) const = 0;
+	};
+	
+	template<typename T>
+	struct Impl : public Concept
+	{
+		Impl(const T& data):mData(data){}
+
+		void print_(wostream& os) const override
+		{
+			print(mData, os);
+		}
+
+		T mData;
+	};
+
+
+private:
+	shared_ptr<Concept> Expression_;
 };
 
 
-
-Object read(vector<Token> tokens)
+struct Number
 {
+	Number(int value): value_(value){}
 
-	auto currentTokenItr = tokens.begin();
+	Expression eval() const { return Expression(*this); }
 
-	if(*currentTokenItr == openParen)
+	operator int() const { return value_; }
+
+	void print_(wostream& os) const
 	{
-		auto listOfObjects = Object();
+		os << value_;
+	}
 
-		currentTokenItr++;
+private:
+	int value_;
+};
 
-		return Object(listOfObjects);
+Expression make_atom(Token token)
+{
+	if(IsNumber(token))
+	{
+		wstringstream ss(token);
+		
+		int num = 0;
+		
+		ss >> num;
 
+		return num;
 	}
 	else
 	{
-		return Object(Type::NUMBER, tokens.front());
+		return token;
 	}
 }
 
 
-void print(Object)
-{}
+struct List
+{
+	List(vector<Token>::iterator& itr, const vector<Token>::iterator& last)
+	{
+		while(itr != last)
+		{
+			if(*itr == openParen)
+			{
+				list_.emplace_back(List(++itr, last));
+			}
+			else if(*itr == closeParen)
+			{
+				itr++;
+				return;
+			}
+			else
+			{
+				list_.push_back(make_atom(*itr));
+				itr++;
+			}
+		}
+	}
 
-wstringstream program(L" 10 ( hello?) (hello -5) ( hi ( one two))  ");
-wstringstream program_norwig(L"(begin (define r 10) (* pi (* r r)))");
+	Expression eval() const { return Expression(0); }
+
+	void print_(wostream& os) const
+	{
+		os << "[";
+
+		for(const auto& expr : list_)
+		{
+			print(expr, os);
+			os << ' ';
+		}
+
+		os << "]";
+	}
+
+private:
+	vector<Expression> list_;
+};
+
+wostream& operator<<(wostream& os, const List& listExpr)
+{
+	listExpr.print_(os);
+
+	return os;
+}
+
+typedef map<wstring, Expression> Context;
+
+
+
+
+struct OpPlus
+{
+	void print_(wostream& os) const
+	{
+		os << L"+";
+	}
+
+	Expression operator()(vector<Expression> operands)
+	{
+		int sum = 0;
+
+		for(const auto& operand : operands)
+		{
+			sum += operand.get<int>();
+		}
+
+		return sum;
+	}
+
+};
+
+wostream& operator<<(wostream& os, const OpPlus& op)
+{
+	op.print_(os);
+
+	return os;
+}
+
+
+
+
+
+
+typedef vector<Expression> Program;
+
+
 
 int main(int argc, char** argv)
 {
+	Context gContext;
 
-	while(1)
+	gContext.emplace(wstring(L"+"), Expression(OpPlus()));
+
+	Program program;
+
+	program.push_back(1);
+	program.push_back(wstring(L"Hello"));
+	program.push_back(Number(5));
+
+
+	for(const auto& aExpr : program)
 	{
-		wstring prog;
-
-		getline(wcin, prog);
-
-		wstringstream ss(prog);
-	
-		auto o = read(tokenize(ss));
-
-		o.print(wcout);
-
-		cout << '\n';
+		print(aExpr, wcout);
 	}
+
+	wcout << program.front().get<int>() << '\n';
+
+	//Program toSum = { 1, 2, 3 , wstring(L"Hello")};
+	Program toSum = { 1, 2, 3 };
+
+	auto op = OpPlus();
+
+	auto sumExpr = op(toSum);
+
+	wcout << L"sum = ";
 	
+	print(sumExpr, wcout);
+	
+	wcout << '\n';
+
+	auto aTokens = tokenize(program_norwig);
+
+	auto myList = List(aTokens.begin(), aTokens.end());
+
+	print(myList, wcout);
+
+	wcout << '\n';
+
+	aTokens = tokenize(program_test);
+
+	auto myList2 = List(aTokens.begin(), aTokens.end());
+
+	print(myList2, wcout);
+
+	wcout << '\n';
+
+
+
+
 	return 0;
-}
-
-inline Object::Object(const TokenItr valueToken)
-{
-	if(IsNumber(*valueToken))
-	{
-		wstringstream ss(*valueToken);
-
-		int value;
-
-		ss >> value;
-
-		if(ss.good())
-		{
-			mpImpl = make_shared<Int>(value);
-		}
-	}
-	else if(*valueToken == openParen)
-	{
-		mpImpl = make_shared<List>();
-	}
-	else
-	{
-		mpImpl = make_shared<Symbol>(valueToken);
-	}
 }
